02c41bd6856b94e4e7bd47a5fb9486a479c4f599
Estevaosouza-oss <155601957+Estevaosouza-oss@users.noreply.github.com>
2025-05-08 22:05:59 -0300

    Add files via upload

---
+++ exercicio031.py
@@ -0,0 +1,8 @@
+viagem = float(input("Qual a Distancia da Kilometragem: "))
+
+if viagem < 200 :
+    vvia = viagem * 0.45 
+    print ("Voce vai viajar {} Km entao voce pagara R$0,45 por KM \nEntão voce tera que pagar R${:.2f} Pela Viagem".format(viagem, vvia))
+else :
+    viag = viagem * 0.5 
+    print ('Voce vai viajar {} Km entao voce pagara R$0,50 por KM \nEntão voce tera que pagar R${:.2f} Pela Viagem'.format(viagem, viag))
